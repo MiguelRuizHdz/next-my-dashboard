d5e4c64cf551b4310f17780af815b9a508406bc5
MiguelRuizHdz <miguel.ruiz.hdz.00@gmail.com>
2024-01-19 07:57:11 -0600

    Resolución de la tarea

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -22,12 +22,17 @@ export default async function PokemonsPage() {
     <div className="flex flex-col">
       
       <div className="flex flex-wrap gap-10 items-center justify-center">
-        <Image 
-          src={ `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${ 10 }.svg` }
-          width={ 100 }
-          height={ 100 }
-          alt='nomnbre'
-        />
+        {
+          pokemons.map( pokemon => (
+            <Image
+              key={ pokemon.id }
+              src={ `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${ pokemon.id }.svg` }
+              width={ 100 }
+              height={ 100 }
+              alt={ pokemon.name }
+            />
+          ))
+        }
       </div>
 
     </div>
