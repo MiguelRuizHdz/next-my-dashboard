7b9b78af66a34942e1052cec2fd4737d64b234f6
MiguelRuizHdz <miguel.ruiz.hdz.00@gmail.com>
2024-01-24 07:52:04 -0600

    Cargar información del Pokémon por ID

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,17 +1,30 @@
+import { Pokemon } from '../../../../pokemons/interfaces/pokemon';
 
 
 interface Props {
   params: { id: string };
-
 }
 
-export default function PokemonPage({ params }: Props) {
+const getPokemon = async(id: string): Promise<Pokemon> => {
+  const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${ id }`, {
+    cache: "force-cache" // TODO: cambiar esto en un futuro
+  }).then( resp => resp.json() );
 
-  console.log({params});
+  console.log('Se cargó: ', pokemon.name);
+
+  return pokemon
+}
+
+export default async function PokemonPage({ params }: Props) {
+
+  const pokemon = await getPokemon(params.id);
   
   return (
     <div>
       <h1>Pokemon { params.id }</h1>
+      <div>
+        { JSON.stringify( pokemon )}
+      </div>
     </div>
   );
 }
--- src/pokemons/index.ts
+++ src/pokemons/index.ts
@@ -1,3 +1,5 @@
+export { PokemonGrid } from './components/PokemonGrid';
+
 export type { PokemonResponse } from './interfaces/pokemons-response';
 export type { SimplePokemon } from './interfaces/simple-pokemon';
-export { PokemonGrid } from './components/PokemonGrid';
+export type { Pokemon } from './interfaces/pokemon';
