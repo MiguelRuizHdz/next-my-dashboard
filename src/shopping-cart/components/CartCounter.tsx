98ca627cfe87d3db485ffd554d6ca503a441d07a
MiguelRuizHdz <miguel.ruiz.hdz.00@gmail.com>
2024-02-02 08:16:58 -0600

    Counter Reducer y acciones

--- src/shopping-cart/components/CartCounter.tsx
+++ src/shopping-cart/components/CartCounter.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { useAppDispatch, useAppSelector } from '@/store';
+import { addOne, substractOne } from '@/store/counter/counterSlice';
 import { useState } from 'react';
 
 interface Props {
@@ -7,7 +9,8 @@ interface Props {
 
 export const CartCounter = ({ value = 0 }: Props) => {
 
-  const [count, setCount] = useState(value);
+  const count = useAppSelector( state => state.counter.count );
+  const dispatch = useAppDispatch();
 
   return (
     <>
@@ -15,12 +18,12 @@ export const CartCounter = ({ value = 0 }: Props) => {
 
       <div className="flex">
         <button
-          onClick={() => setCount(count + 1)}
+          onClick={() => dispatch( addOne() )}
           className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">
           +1
         </button>
         <button
-          onClick={() => setCount(count - 1)}
+          onClick={() => dispatch( substractOne() )}
           className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">
           -1
         </button>
